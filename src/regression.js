50d9604eacfcef0d77397292d314240c336db4b6
satya23k <satyakonidala@gmail.com>
2015-12-03 18:01:01 +0530

    Update regression.js
    updated:
    1.included rSquare output from linear and logarithmic function
    2.increased precsion of claulated value to 4 place
    3.included moving average.

--- src/regression.js
+++ src/regression.js
@@ -56,16 +56,18 @@
 
                 var gradient = (n * sum[3] - sum[0] * sum[1]) / (n * sum[2] - sum[0] * sum[0]);
                 var intercept = (sum[1] / n) - (gradient * sum[0]) / n;
-              //  var correlation = (n * sum[3] - sum[0] * sum[1]) / Math.sqrt((n * sum[2] - sum[0] * sum[0]) * (n * sum[4] - sum[1] * sum[1]));
-
+               var correlation = (n * sum[3] - sum[0] * sum[1]) / Math.sqrt((n * sum[2] - sum[0] * sum[0]) * (n * sum[4] - sum[1] * sum[1]));
+               var rSquare = Math.pow(correlation,2)
                 for (var i = 0, len = data.length; i < len; i++) {
                     var coordinate = [data[i][0], data[i][0] * gradient + intercept];
                     results.push(coordinate);
                 }
+               
+               	 if(intercept<0){var sign = "-"} 
+               	 		else {var sign = "+"};
+                var string = 'y = ' + Math.round(gradient*10000) / 10000 + 'x '+sign+" " + Math.abs(Math.round(intercept*10000) / 10000);
 
-                var string = 'y = ' + Math.round(gradient*100) / 100 + 'x + ' + Math.round(intercept*100) / 100;
-
-                return {equation: [gradient, intercept], points: results, string: string};
+                return {equation: [gradient, intercept], points: results, string: string,correlation:correlation,rSquare:rSquare};
             },
 
             linearThroughOrigin: function(data) {
@@ -85,7 +87,7 @@
                     results.push(coordinate);
                 }
 
-                var string = 'y = ' + Math.round(gradient*100) / 100 + 'x';
+                var string = 'y = ' + Math.round(gradient*10000) / 10000 + 'x';
 
                 return {equation: [gradient], points: results, string: string};
             },
@@ -113,13 +115,13 @@
                     results.push(coordinate);
                 }
 
-                var string = 'y = ' + Math.round(A*100) / 100 + 'e^(' + Math.round(B*100) / 100 + 'x)';
+                var string = 'y = ' + Math.round(A*10000) / 10000 + 'e^(' + Math.round(B*10000) / 10000 + 'x)';
 
                 return {equation: [A, B], points: results, string: string};
             },
 
             logarithmic: function(data) {
-                var sum = [0, 0, 0, 0], n = 0, results = [];
+                var sum = [0, 0, 0, 0, 0], n = 0, results = [];
 
                 for (len = data.length; n < len; n++) {
                   if (data[n][1] != null) {
@@ -127,20 +129,27 @@
                     sum[1] += data[n][1] * Math.log(data[n][0]);
                     sum[2] += data[n][1];
                     sum[3] += Math.pow(Math.log(data[n][0]), 2);
+                    sum[4] += Math.pow(data[n][1], 2);
                   }
                 }
 
                 var B = (n * sum[1] - sum[2] * sum[0]) / (n * sum[3] - sum[0] * sum[0]);
                 var A = (sum[2] - B * sum[0]) / n;
-
+                
+                var correlation = (n * sum[1] - sum[2] * sum[0]) / Math.sqrt((n * sum[3] - sum[0] * sum[0]) * (n * sum[4] - sum[2] * sum[2]));
+                var rSquare = Math.pow(correlation,2)
+                
                 for (var i = 0, len = data.length; i < len; i++) {
                     var coordinate = [data[i][0], A + B * Math.log(data[i][0])];
                     results.push(coordinate);
                 }
+                
+                if(A<0){var sign = "-"} 
+       	 		else {var sign = "+"};
+       	 		
+                var string = 'y = ' + Math.round(B*10000) / 10000 + ' ln(x) ' + sign+" " + Math.abs(Math.round(A*10000) / 10000) ;
 
-                var string = 'y = ' + Math.round(A*100) / 100 + ' + ' + Math.round(B*100) / 100 + ' ln(x)';
-
-                return {equation: [A, B], points: results, string: string};
+                return {equation: [A, B], points: results, string: string,rSquare:rSquare};
             },
 
             power: function(data) {
@@ -163,7 +172,7 @@
                     results.push(coordinate);
                 }
 
-                 var string = 'y = ' + Math.round(A*100) / 100 + 'x^' + Math.round(B*100) / 100;
+                 var string = 'y = ' + Math.round(A*10000) / 10000 + 'x^' + Math.round(B*10000) / 10000;
 
                 return {equation: [A, B], points: results, string: string};
             },
@@ -208,13 +217,29 @@
 
                     for(var i = equation.length-1; i >= 0; i--){
                       if(i > 1) string += Math.round(equation[i] * Math.pow(10, i)) / Math.pow(10, i)  + 'x^' + i + ' + ';
-                      else if (i == 1) string += Math.round(equation[i]*100) / 100 + 'x' + ' + ';
-                      else string += Math.round(equation[i]*100) / 100;
+                      else if (i == 1) string += Math.round(equation[i]*10000) / 10000 + 'x' + ' + ';
+                      else string += Math.round(equation[i]*10000) / 10000;
                     }
 
                 return {equation: equation, points: results, string: string};
             },
-
+            movingAverage: function(data,period) {
+            	var result=[],nums = [];
+        		  for (var i in data) {
+        		        nums.push(data[i]);
+        		        if (nums.length > period)
+        		            nums.splice(0,1);  // remove the first element of the array
+        		        var sum = 0;
+        		        for (var i in nums)
+        		            sum += nums[i];
+        		        var n = period;
+        		        if (nums.length < period)
+        		            n = nums.length;
+        		      result.push(sum/n);
+        		    }
+        		  result.splice(0,period-1)   //removed avg which are same as data passed
+        		  return result
+            },
             lastvalue: function(data) {
               var results = [];
               var lastvalue = null;
